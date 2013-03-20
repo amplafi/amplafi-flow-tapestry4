d8e698c422766b912a1bd477971c087268859c42
Konstantin Burov <aectann@gmail.com>
2013-03-21 00:27:12 +0900

    Compilation fix.

--- src/main/java/org/amplafi/flow/web/services/HtmlFlowRenderer.java
+++ src/main/java/org/amplafi/flow/web/services/HtmlFlowRenderer.java
@@ -2,7 +2,9 @@ package org.amplafi.flow.web.services;
 
 import java.io.Writer;
 
+import org.amplafi.flow.Flow;
 import org.amplafi.flow.FlowConstants;
+import org.amplafi.flow.FlowManagement;
 import org.amplafi.flow.FlowRenderer;
 import org.amplafi.flow.FlowState;
 import org.amplafi.flow.web.FlowWebUtils;
@@ -46,13 +48,21 @@ public class HtmlFlowRenderer implements FlowRenderer {
 	}
 
 	@Override
-	public void describeFlow(Writer writer, String flowType) {
-	}
-
-	@Override
 	public void render(Writer writer, FlowState flowState, String errorMessage,
 			Exception exception) {
 		render(flowState);
 	}
+
+    @Override
+    public void describeFlow(Writer writer, Flow flowType) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void describeApi(Writer writer, FlowManagement flowManagement) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
